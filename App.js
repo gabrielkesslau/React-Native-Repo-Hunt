afb14987057a33e1d7ac51b419606d6e5348585f
Gabriel Kesslau <gabrielkesslau@gmail.com>
2020-04-10 19:02:45 -0300

    Repo Hunt

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './src/routes';
+import './src/config/StatusBarConfig';
+//import { Text, View } from 'react-native';
+
+
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
